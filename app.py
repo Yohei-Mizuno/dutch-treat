0389d2f5c4fa62072e814c6e1dd7129162a915c0
Yohei-Mizuno <mizyotama2525@gmail.com>
2020-08-24 22:16:11 +0900

    テーブルごとで使い回せる割勘アプリを作成｡
    total_amount:総支払金額
    number_of_people:支払い人数
    amount_per_person:一人あたりの支払金額
    fraction:端数
    
    app.py上でprint関数で表示することは可能
    
    アプリケーションとしては未完成

---
+++ app.py
@@ -0,0 +1,21 @@
+# table1: 1500, 3人
+# table2: 2000, 3人
+# table3: 3647, 4人
+
+total_amount = 1500
+number_of_people = 3
+
+amount_per_person = total_amount // number_of_people
+
+print(f'一人あたり:{amount_per_person}円, 端数:{total_amount % number_of_people}円')
+
+
+def dutch_treat(total_amount, number_of_people):
+    amount_per_person = total_amount // number_of_people
+    fraction = total_amount % number_of_people
+    return f"一人あたりの金額:{amount_per_person}円\n端数:{fraction}円"
+
+
+print(dutch_treat(1500, 3))
+print(dutch_treat(2000, 3))
+print(dutch_treat(3647, 4))
